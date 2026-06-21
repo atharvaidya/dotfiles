e693d97cf02a23c71a03d27dc38932df10676795
Atharvaidya <vaiddya.atharva@gmail.com>
2026-06-21 23:12:55 +0530

    Bar Customised

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -11,7 +11,5 @@ hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd("quickshell")
-	hl.exec_cmd("sudo pacman -Syu")
-	hl.exec_cmd("yay")
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 end)
